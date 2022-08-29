21cec219d7d372dfc6848e489a27f7205077f99d
khumayunkh <hasanovhumayun15@gmail.com>
2022-08-30 04:09:34 +0500

    title

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header/Header';
+import Title from './components/Title/Title';
 
 function App() {
   return (
     <>
     <Header/>
+    <Title/>
     </>
   );
 }
---
+++ src/components/Title/Title.js
@@ -0,0 +1,17 @@
+import React from "react";
+import style from './title.module.css'
+import photo from './../../images/title.svg'
+
+function Title(){
+    return(
+        <>
+        <div className={style.container}>
+            <div className={style.title}>
+                <img src={photo}/>
+            </div> 
+        </div>
+        </>
+    )
+}
+
+export default Title
