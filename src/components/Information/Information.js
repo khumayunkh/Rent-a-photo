067fee23323dd6388c183b4ed53b5b3e18d415ee
khumayunkh <hasanovhumayun15@gmail.com>
2022-08-30 18:43:54 +0500

    styl

--- src/components/Information/Information.js
+++ src/components/Information/Information.js
@@ -9,28 +9,30 @@ function Information(){
             <div className={style.information}>
                 <h2>Цена</h2>
                 <div className={style.information_in}>
-                    <h4 className={style.title}>Будний</h4>
-                    <h4 className={style.price}>1 880$</h4>
+                    <div className={style.title_in}>
+                        <h4 className={style.title}>Будний</h4>
+                    </div>
+                    <div className={style.price_in}><h4 className={style.price}>1 880$</h4>
                     <h4 className={style.price}>990$</h4>
-                    <h4 className={style.price}>1 200$</h4>
+                    <h4 className={style.price}>1 200$</h4></div>
                 </div>
                 <div className={style.information_in}>
-                    <h4 className={style.title}>Выходной</h4>
-                    <h4 className={style.price}>1 950$</h4>
+                   <div className={style.title_in}><h4 className={style.title}>Выходной</h4></div> 
+                   <div className={style.price_in}><h4 className={style.price}>1 950$</h4>
                     <h4 className={style.price}>1 100$</h4>
-                    <h4 className={style.price}>1 250$</h4>
+                    <h4 className={style.price}>1 250$</h4></div>
                 </div>
                 <div className={style.information_in}>
-                    <h4 className={style.title}>Неделя</h4>
-                    <h4 className={style.price}>10 950$</h4>
+                    <div className={style.title_in}><h4 className={style.title}>Неделя</h4></div> 
+                    <div className={style.price_in}><h4 className={style.price}>10 950$</h4>
                     <h4 className={style.price}>9 100$</h4>
-                    <h4 className={style.price}>7 250$</h4>
+                    <h4 className={style.price}>7 250$</h4></div>
                 </div>
                 <div className={style.information_in}>
-                    <h4 className={style.title}>Месяц</h4>
-                    <h4 className={style.price}>29 950$</h4>
+                   <div className={style.title_in}><h4 className={style.title}>Месяц</h4></div> 
+                   <div className={style.price_in}> <h4 className={style.price}>29 950$</h4>
                     <h4 className={style.price}>18 100$</h4>
-                    <h4 className={style.price}>15 250$</h4>
+                    <h4 className={style.price}>15 250$</h4></div>
                 </div>
             </div>
         </div>
