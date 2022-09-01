5076cc7db51354f08f59ffbd04d87a073c287a28
khumayunkh <hasanovhumayun15@gmail.com>
2022-09-02 02:37:47 +0500

    style

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -4,6 +4,7 @@ import logo from './../../images/logo.png'
 
 
 function Contact(){
+    
     return(
         <>
         <div className={style.container}>
@@ -16,16 +17,33 @@ function Contact(){
                     <h4>2015-2019 Fotoprokat24</h4>
                 </div>
                 <div className={style.company}>
-                    
+                    <h3>Компания</h3>
+                    <h4>О компании</h4>
+                    <h4>Новости</h4>
+                    <h4>Вакансии</h4>
+                    <h4>Политика</h4>
+                    <h4>Конфиденциальность</h4>
                 </div>
                 <div className={style.client}>
-
+                    <h3>Клиентам</h3>
+                    <h4>Точки выдачи</h4>
+                    <h4>Скидки</h4>
+                    <h4>Бонусные программы</h4>
                 </div>
                 <div className={style.help}>
-
+                    <h3>Помощь</h3>
+                    <h4>Вопрос-ответ</h4>
+                    <h4>Правила аренды</h4>
+                    <h4>Доставка</h4>
+                    <h4>Оплата</h4>
                 </div>
-                <div className={style.contact}>
-
+                <div className={style.contact_us}>
+                    <h3>Кантакты</h3>
+                    <h4>Fotoprokat24</h4>
+                    <h4>г.Москве, Проспект мира,
+                         дом 12 </h4>
+                    <h4>Пн-Вс 9:00-12:00</h4>
+                    <h4>+7 (999) 999-99-99</h4> 
                 </div>
             </div>
         </div>
