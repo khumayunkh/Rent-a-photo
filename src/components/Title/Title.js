56c098b89812d0f3b48560f7a614cfbed936011e
khumayunkh <hasanovhumayun15@gmail.com>
2022-09-07 02:36:56 +0500

    style

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -2,14 +2,17 @@ import React from "react";
 import style from './title.module.css'
 import photo from './../../images/title.svg'
 
-function Title(){
-    
+function Title(){ 
     return(
         <>
         <div className={style.container}>
             <div className={style.title}>
                 <img src={photo}/>
             </div> 
+            <div className={style.title_mobile}>
+                <h2>Главная/Сравнить товары</h2>
+                <h1>Сравнить 5 товаров</h1>
+            </div>
         </div>
         </>
     )
